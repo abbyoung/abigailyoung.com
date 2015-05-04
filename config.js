3df44ab9a5aceb69b52717ec93e4e3261c03a6f0
Abigail <ayoung@sfo2ayoung315.gpjco.com>
2015-05-04 15:22:16 -0700

    fixing base url

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://guarded-shelf-1674.herokuapp.com',
+        url: 'http://abigailyoung.com',
         fileStorage: false,
         mail: {},
         database: {
